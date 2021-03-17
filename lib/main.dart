d3d08430977ebb4d81a68a444e0e91cee8774a05
Your Name <you@example.com>
2021-03-17 15:02:39 +0200

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:game_lap/Screens/splash_screen.dart';
+main(){
+  runApp(Home());
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
